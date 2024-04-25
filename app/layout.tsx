a7c02e977997f9a8ecf545d5ba3317d9b0afe89a
svalbardexperts <167121041+svalbardexperts@users.noreply.github.com>
2024-04-25 20:42:21 +0000

    tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-KD7P6SX9');`
+          }}
+        />
+      </head>
       <body className={poppins.className}>
         <main className='relative'>
           {/* watsapp widget */}
